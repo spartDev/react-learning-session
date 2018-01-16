d867384db318c0b5c1857040bfe3beb0c39280c7
Thomas Roux <troux@worldia.com>
2018-01-16 15:36:43 +0100

    feat(component): create a counter component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Counter from './Counter';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -10,9 +12,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* Here our counter component */}
+        <Counter />
+
+        {/* We can set our component width props (initial data) */}
+        {/* <Counter SetcountNumberTo={4}/> */}
       </div>
     );
   }
--- src/Counter.js
+++ src/Counter.js
@@ -1,1 +1,68 @@
+import React, { Component } from 'react';
+import './Counter.css';
 
+class Counter extends Component {
+  constructor() {
+    super();
+     this.state = {
+         count: 0
+     };
+     this.handleCountUp = this.handleCountUp.bind(this);
+     this.handleCountDown = this.handleCountDown.bind(this);
+   }
+
+   // Initializing state inside of the constructor comes with an overhead
+   // of calling super and remembering about props which are React’s abstraction
+   // which leaks here a little. We can initialize state directly as a class property.
+
+   // Due to the fact that the state is defined on the instance,
+   // we still have access to this during state initialization.
+   // This makes it possible to use props to initialize the state
+
+   // state = {
+   //   count: 0,
+   // }
+
+   // state = {
+   //   count: this.props.SetcountNumberTo,
+   // }
+
+   handleCountUp() {
+     this.setState({
+       count: this.state.count + 1
+    });
+   }
+
+   handleCountDown() {
+     this.setState({
+       count: this.state.count - 1
+    });
+   }
+
+   // We can benefit from the fact that arrow function preserves context
+   // in which was defined and set handler directly as a class field.
+
+   // handleCountUp = () => {
+   //   this.setState({
+   //     count: this.state.count + 1
+   //  });
+   // }
+   //
+   // handleCountDown = () => {
+   //   this.setState({
+   //     count: this.state.count - 1
+   //  });
+   // }
+
+  render() {
+    return (
+      <div className="countComponent">
+        <button onClick={this.handleCountDown} className="countComponent__down">Count Down</button>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.handleCountUp} className="countComponent__up">Count Up</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
